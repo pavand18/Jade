535f577fac54cbd2cabe781acece7bff23cd317f
pavand18 <pavandakore@gmail.com>
2024-04-24 22:12:58 +0530

    basic of app.py done

--- backend/app.py
+++ backend/app.py
@@ -364,19 +364,25 @@ def dopca():
         "data": first_four_rows
     })
 
-
 @app.route('/reconstruct', methods=['GET'])
 def reconstruct():
     X_pca = g_X_pca
     principal_components = g_principal_components
-    X = g_X
+    mean_std_file = g_mean_std_file
+    mean_std_dev_data = pd.read_csv(mean_std_file)
+
+    input_file_path = mean_std_file # Replace with your input file path
+    df = pd.read_csv(input_file_path)
+
+    first_column = df.iloc[:, 0]
+    column_names_list = first_column.tolist()
 
     reconstructed_data = np.dot(X_pca, principal_components.T)
-    reconstructed_df = pd.DataFrame(reconstructed_data, columns=X.columns)
+    reconstructed_df = pd.DataFrame(reconstructed_data, columns=column_names_list)
 
     filename = os.path.basename(input_modified_file)
     reconstructed_file_path = os.path.join(app.config['UPLOAD_FOLDER'], "reconstructed_" + filename)
-    reconstructed_df.to_csv(reconstructed_file_path, index=False)
+    reconstructed_df.to_csv(reconstructed_file_path, index=False, float_format='%.4f')
 
     global reconstructed_file
     reconstructed_file = reconstructed_file_path #global file
@@ -386,7 +392,6 @@ def reconstruct():
     X_reconstructed_data = pd.read_csv(fake_output_file)
 
     first_four_rows = X_reconstructed_data.head(4).to_dict(orient='records')
-    # first_four_rows = X_reconstructed_data(4).apply(lambda row: row[:20].to_dict(), axis=1).tolist()
     return jsonify({
         "success": True,
         "message": "File uploaded and processed successfully",
@@ -412,12 +417,9 @@ def original():
     # Reverse the standardization to obtain the original data
     original_data = reverse_standardization(reconstructed_data, means, stds)
 
-    # Output CSV file path for original data
-    # output_csv_file = 'original_data.csv'
-
     filename = os.path.basename(input_modified_file)
     original_csv_file = os.path.join(app.config['UPLOAD_FOLDER'], "original_" + filename)
-    original_data.to_csv(original_csv_file, index=False)
+    original_data.to_csv(original_csv_file, index=False, float_format='%.4f')
     global g_original_file 
     g_original_file = original_csv_file
 
@@ -445,265 +447,21 @@ def original():
         for parameter, value in parameters.items():
             print(f"- {parameter}: {value}")
 
-    # return send_file(original_csv_file, as_attachment=True)
-
     add_row_to_csv(original_csv_file, fake_output_file)
     X_original_data = pd.read_csv(fake_output_file)
 
     first_four_rows = X_original_data.head(4).to_dict(orient='records')
-    # first_four_rows = X_original_data(4).apply(lambda row: row[:20].to_dict(), axis=1).tolist()
-
     return jsonify({
         "success": True,
         "message": "File uploaded and processed successfully",
         "data": first_four_rows
     })
 
-# @app.route('/original', methods=['GET'])
-# def original():
-#     # Input CSV file paths
-#     reconstructed_data_csv_file = reconstructed_file
-#     mean_std_dev_csv_file = g_mean_std_file
-
-#     # Read the standardized data from the CSV file
-#     reconstructed_data = pd.read_csv(reconstructed_data_csv_file)
-
-#     # Read means and standard deviations from the CSV file
-#     mean_std_dev_data = pd.read_csv(mean_std_dev_csv_file)
-
-#     # Extract means and standard deviations
-#     means = mean_std_dev_data['Mean'].values
-#     stds = mean_std_dev_data['Standard_Deviation'].values
-
-#     # Reverse the standardization to obtain the original data
-#     original_data = reverse_standardization(reconstructed_data, means, stds)
-
-#     # Output CSV file path for original data
-#     # output_csv_file = 'original_data.csv'
-
-#     filename = os.path.basename(input_modified_file)
-#     original_csv_file = os.path.join(app.config['UPLOAD_FOLDER'], "original_" + filename)
-#     original_data.to_csv(original_csv_file, index=False)
-#     global g_original_file 
-#     g_original_file = original_csv_file
-
-#     # Original data
-#     original_data_str = """Bus ODESSA 2 0 V pu,Bus ODESSA 2 0 V angle,Bus ODESSA 2 0 Frequency,Bus PRESIDIO 2 0 V pu,Bus PRESIDIO 2 0 V angle,Bus PRESIDIO 2 0 Frequency,Bus O DONNELL 1 0 V pu,Bus O DONNELL 1 0 V angle,Bus O DONNELL 1 0 Frequency,Bus O DONNELL 1 1 V pu,Bus O DONNELL 1 1 V angle,Bus O DONNELL 1 1 Frequency,Bus BIG SPRING 5 0 V pu,Bus BIG SPRING 5 0 V angle,Bus BIG SPRING 5 0 Frequency,Bus BIG SPRING 5 1 V pu,Bus BIG SPRING 5 1 V angle,Bus BIG SPRING 5 1 Frequency,Bus VAN HORN 0 V pu,Bus VAN HORN 0 V angle,Bus VAN HORN 0 Frequency,Bus IRAAN 2 0 V pu,Bus IRAAN 2 0 V angle,Bus IRAAN 2 0 Frequency,Bus IRAAN 2 1 V pu,Bus IRAAN 2 1 V angle,Bus IRAAN 2 1 Frequency,Bus PRESIDIO 1 0 V pu,Bus PRESIDIO 1 0 V angle,Bus PRESIDIO 1 0 Frequency,Bus PRESIDIO 1 1 V pu,Bus PRESIDIO 1 1 V angle,Bus PRESIDIO 1 1 Frequency,Bus SANDERSON 0 V pu,Bus SANDERSON 0 V angle,Bus SANDERSON 0 Frequency,Bus MONAHANS 2 0 V pu,Bus MONAHANS 2 0 V angle,Bus MONAHANS 2 0 Frequency,Bus GRANDFALLS 0 V pu,Bus GRANDFALLS 0 V angle,Bus GRANDFALLS 0 Frequency,Bus MARFA 0 V pu,Bus MARFA 0 V angle,Bus MARFA 0 Frequency,Bus GARDEN CITY 0 V pu,Bus GARDEN CITY 0 V angle,Bus GARDEN CITY 0 Frequency,Bus ODESSA 4 0 V pu,Bus ODESSA 4 0 V angle,Bus ODESSA 4 0 Frequency,Bus NOTREES 0 V pu,Bus NOTREES 0 V angle,Bus NOTREES 0 Frequency,Bus MIDLAND 4 0 V pu,Bus MIDLAND 4 0 V angle,Bus MIDLAND 4 0 Frequency,Bus BIG SPRING 1 0 V pu,Bus BIG SPRING 1 0 V angle,Bus BIG SPRING 1 0 Frequency,Bus BIG SPRING 1 1 V pu,Bus BIG SPRING 1 1 V angle,Bus BIG SPRING 1 1 Frequency,Bus O DONNELL 2 0 V pu,Bus O DONNELL 2 0 V angle,Bus O DONNELL 2 0 Frequency,Bus O DONNELL 2 1 V pu,Bus O DONNELL 2 1 V angle,Bus O DONNELL 2 1 Frequency,Bus ODESSA 6 0 V pu,Bus ODESSA 6 0 V angle,Bus ODESSA 6 0 Frequency,Bus BIG SPRINGS 0 V pu,Bus BIG SPRINGS 0 V angle,Bus BIG SPRINGS 0 Frequency,Bus BIG SPRINGS 1 V pu,Bus BIG SPRINGS 1 V angle,Bus BIG SPRINGS 1 Frequency,Bus MIDLAND 2 0 V pu,Bus MIDLAND 2 0 V angle,Bus MIDLAND 2 0 Frequency,Bus COAHOMA 0 V pu,Bus COAHOMA 0 V angle,Bus COAHOMA 0 Frequency,Bus MIDLAND 3 0 V pu,Bus MIDLAND 3 0 V angle,Bus MIDLAND 3 0 Frequency,Bus ALPINE 0 V pu,Bus ALPINE 0 V angle,Bus ALPINE 0 Frequency,Bus FORT DAVIS 0 V pu,Bus FORT DAVIS 0 V angle,Bus FORT DAVIS 0 Frequency,Bus MCCAMEY 1 0 V pu,Bus MCCAMEY 1 0 V angle,Bus MCCAMEY 1 0 Frequency,Bus KERMIT 0 V pu,Bus KERMIT 0 V angle,Bus KERMIT 0 Frequency,Bus ODESSA 1 0 V pu,Bus ODESSA 1 0 V angle,Bus ODESSA 1 0 Frequency,Bus ALPINE 1 0 V pu,Bus ALPINE 1 0 V angle,Bus ALPINE 1 0 Frequency,Bus ALPINE 1 1 V pu,Bus ALPINE 1 1 V angle,Bus ALPINE 1 1 Frequency,Bus MARFA 1 0 V pu,Bus MARFA 1 0 V angle,Bus MARFA 1 0 Frequency,Bus MARFA 1 1 V pu,Bus MARFA 1 1 V angle,Bus MARFA 1 1 Frequency,Bus MIDLAND 1 0 V pu,Bus MIDLAND 1 0 V angle,Bus MIDLAND 1 0 Frequency,Bus SEMINOLE 0 V pu,Bus SEMINOLE 0 V angle,Bus SEMINOLE 0 Frequency,Bus BIG SPRING 3 0 V pu,Bus BIG SPRING 3 0 V angle,Bus BIG SPRING 3 0 Frequency,Bus ODESSA 5 0 V pu,Bus ODESSA 5 0 V angle,Bus ODESSA 5 0 Frequency,Bus BIG SPRING 4 0 V pu,Bus BIG SPRING 4 0 V angle,Bus BIG SPRING 4 0 Frequency,Bus ODESSA 3 0 V pu,Bus ODESSA 3 0 V angle,Bus ODESSA 3 0 Frequency,Bus ODESSA 3 1 V pu,Bus ODESSA 3 1 V angle,Bus ODESSA 3 1 Frequency,Bus BIG SPRING 2 0 V pu,Bus BIG SPRING 2 0 V angle,Bus BIG SPRING 2 0 Frequency,Bus ALPINE 2 0 V pu,Bus ALPINE 2 0 V angle,Bus ALPINE 2 0 Frequency,Bus MARFA 2 0 V pu,Bus MARFA 2 0 V angle,Bus MARFA 2 0 Frequency,Bus MIDLAND 0 V pu,Bus MIDLAND 0 V angle,Bus MIDLAND 0 Frequency,Bus MONAHANS 1 0 V pu,Bus MONAHANS 1 0 V angle,Bus MONAHANS 1 0 Frequency,Bus MCCAMEY 2 0 V pu,Bus MCCAMEY 2 0 V angle,Bus MCCAMEY 2 0 Frequency,Bus SEMINOLE 1 0 V pu,Bus SEMINOLE 1 0 V angle,Bus SEMINOLE 1 0 Frequency,Bus MCCAMEY 0 V pu,Bus MCCAMEY 0 V angle,Bus MCCAMEY 0 Frequency,Bus MONAHANS 0 V pu,Bus MONAHANS 0 V angle,Bus MONAHANS 0 Frequency,Bus ODESSA 0 V pu,Bus ODESSA 0 V angle,Bus ODESSA 0 Frequency"""
-
-#     # Parsing the original data
-#     parsed_data = original_data_str.split(',')
-
-#     # Storing data in a dictionary
-#     bus_data = {}
-#     for item in parsed_data:
-#         parts = item.split()
-#         bus_name = ' '.join(parts[1:-3])  # Extracting bus name
-#         parameter = parts[-3]  # Extracting parameter (e.g., V pu, V angle, Frequency)
-#         value = parts[-1]  # Extracting value
-#         if bus_name not in bus_data:
-#             bus_data[bus_name] = {}
-#         bus_data[bus_name][parameter] = value
-
-#     # Printing the formatted output
-#     print("Bus Data:")
-#     for bus, parameters in bus_data.items():
-#         print(bus)
-#         for parameter, value in parameters.items():
-#             print(f"- {parameter}: {value}")
-
-#     return send_file(original_csv_file, as_attachment=True)
-
 def reverse_standardization(reconstructed_data, means, stds):
         # Reverse the standardization
         original_data = (reconstructed_data * stds) + means
         return original_data
 
-#adding plots..
-@app.route('/plot1', methods=['GET'])
-def plot1():
-    original_data_toplot = input_modified_file
-    original_column_data = pd.read_csv(original_data_toplot)
-
-    # specify col. num. here
-    data_from_particular_column = original_column_data.iloc[:, 4]
-    start_index = 0  # Start index of the data points to select
-    end_index = 200  # End index of the data points to select
-    limited_data = data_from_particular_column[start_index:end_index]
-
-    plt.figure(figsize=(6, 4))
-
-    # Plot the limited data
-    plt.plot(limited_data)
-    plt.title('Plot of Input data')
-    plt.xlabel('Index')
-    plt.ylabel('Data')
-    plt.grid(True) 
-
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-
-    plot_url = base64.b64encode(buf.read()).decode()
-
-    return {'plot_url': plot_url}
-
-@app.route('/plot2', methods=['GET'])
-def plot2():
-    original_data_toplot = g_original_file
-    original_column_data = pd.read_csv(original_data_toplot)
-
-    # specify col. num. here
-    data_from_particular_column = original_column_data.iloc[:, 4]
-    start_index = 0  # Start index of the data points to select
-    end_index = 200  # End index of the data points to select
-    limited_data = data_from_particular_column[start_index:end_index]
-
-    plt.figure(figsize=(6, 4))
-
-    # Plot the limited data
-    plt.plot(limited_data)
-    plt.title('Plot of Reconstructed data')
-    plt.xlabel('Index')
-    plt.ylabel('Data')
-    plt.grid(True) 
-
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-
-    plot_url = base64.b64encode(buf.read()).decode()
-
-    return {'plot_url': plot_url}
-
-@app.route('/plot4', methods=['GET'])
-def plot4():
-    eigenvalues = g_eigenvalues
-    plt.figure(figsize=(6, 4))
-    plt.plot(range(1, 10), eigenvalues[:9], marker='o')
-    plt.title('Variance of First 9 Principal Components')
-    plt.xlabel('Principal Component')
-    plt.ylabel('Variance')
-    plt.grid(True)
-
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-
-    plot_url = base64.b64encode(buf.read()).decode()
-
-    return {'plot_url': plot_url}
-
-@app.route('/plot3', methods=['GET'])
-def plot3():
-    file_path_1 = input_modified_file
-    file_path_2 = g_original_file
-    print(f"AAAA filename: {file_path_1}") # Debugging line
-    print(f"BBBB filename: {file_path_2}") # Debugging line
-
-    def load_and_select_data(file_path, column_index, start_index, end_index):
-        reconstructed_column_data = pd.read_csv(file_path)
-        data_from_column = reconstructed_column_data.iloc[:, column_index]
-        return data_from_column[start_index:end_index]
-
-    start_index = 0
-    end_index = 200                         
-    data_from_second_column_1 = load_and_select_data(file_path_1, 4, start_index, end_index)
-    data_from_second_column_2 = load_and_select_data(file_path_2, 4, start_index, end_index)
-
-    plt.figure(figsize=(8, 4))
-
-    # Plot the limited data
-    plt.plot(data_from_second_column_1, label='Input Plot')
-    plt.plot(data_from_second_column_2, label='Reconstructed Plot')
-    plt.title(f'Combined Plot (Indices: {start_index}-{end_index})')
-    plt.xlabel('Index')
-    plt.ylabel('Data')
-    plt.grid(True)
-    plt.legend()  # Add legend to distinguish between the two plots
-
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-
-    plot_url = base64.b64encode(buf.read()).decode()
-    plt.close()  # Close the figure after saving
-
-    return {'plot_url': plot_url}
-
-def load_and_select_data(file_path, column_index, start_index, end_index):
-    reconstructed_column_data = pd.read_csv(file_path)
-    data_from_column = reconstructed_column_data.iloc[:, column_index]
-    return data_from_column[start_index:end_index]
-
-@app.route('/plot5', methods=['GET'])
-def plot5():
-    file_path_1 = standardised_file
-    file_path_2 = reconstructed_file
-    print(f"AAAA filename: {file_path_1}") # Debugging line
-    print(f"BBBB filename: {file_path_2}") # Debugging line
-
-    def load_and_select_data(file_path, column_index, start_index, end_index):
-        reconstructed_column_data = pd.read_csv(file_path)
-        data_from_column = reconstructed_column_data.iloc[:, column_index]
-        return data_from_column[start_index:end_index]
-
-    start_index = 0
-    end_index = 200                         
-    data_from_second_column_1 = load_and_select_data(file_path_1, 4, start_index, end_index)
-    data_from_second_column_2 = load_and_select_data(file_path_2, 4, start_index, end_index)
-
-    plt.figure(figsize=(8, 4))
-
-    # Plot the limited data
-    plt.plot(data_from_second_column_1, label='Standardised Plot')
-    plt.plot(data_from_second_column_2, label='Reconstructed_S Plot')
-    plt.title(f'Combined Plot (Indices: {start_index}-{end_index})')
-    plt.xlabel('Index')
-    plt.ylabel('Data')
-    plt.grid(True)
-    plt.legend()  # Add legend to distinguish between the two plots
-
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-
-    plot_url = base64.b64encode(buf.read()).decode()
-    plt.close()  # Close the figure after saving
-
-    return {'plot_url': plot_url}
-
-def load_and_select_data(file_path, column_index, start_index, end_index):
-    reconstructed_column_data = pd.read_csv(file_path)
-    data_from_column = reconstructed_column_data.iloc[:, column_index]
-    return data_from_column[start_index:end_index]
-
-@app.route('/plot6', methods=['GET'])
-def plot6():
-    original_data_toplot = standardised_file
-    original_column_data = pd.read_csv(original_data_toplot)
-
-    # specify col. num. here
-    data_from_particular_column = original_column_data.iloc[:, 4]
-    start_index = 0  # Start index of the data points to select
-    end_index = 200  # End index of the data points to select
-    limited_data = data_from_particular_column[start_index:end_index]
-
-    plt.figure(figsize=(6, 4))
-
-    # Plot the limited data
-    plt.plot(limited_data)
-    plt.title('Plot of Standardised data')
-    plt.xlabel('Index')
-    plt.ylabel('Data')
-    plt.grid(True) 
-
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-
-    plot_url = base64.b64encode(buf.read()).decode()
-
-    return {'plot_url': plot_url}
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
